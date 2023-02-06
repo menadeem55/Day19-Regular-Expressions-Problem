e21cdf6f1db0a2477983296c71e77028e9b74787
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2023-02-06 23:37:04 +0530

    Added: To Validation of Last Name

--- src/com/bridgelabz/regex/UserRegistration.java
+++ src/com/bridgelabz/regex/UserRegistration.java
@@ -12,10 +12,20 @@ public class UserRegistration {
 	public static void main(String[] args) {
 		System.out.println("welcome to User Registration program");
 		Scanner scanner = new Scanner(System.in);
+		/*
+		 * User need to enter a valid First Name and Last Name First name and Last Name
+		 * start capital minimum 1.
+		 */
 		System.out.println("Enter First Name: ");
 		String firstName = "[A-Z]{1}[a-z]{2,}";
 		String input = scanner.next(); // Nadeem = ture, nadeem = false
+
+		System.out.println("Enter Last Name: ");
+		String lastName = "[A-z]{1}[a-z]{2,}";
+		String inputLastName = scanner.next(); // Akhtar = ture, akhtar = false
+
 		UserRegistration userRegistration = new UserRegistration();
 		System.out.println(userRegistration.userName(firstName, input));
+		System.out.println(userRegistration.userName(lastName, inputLastName));
 	}
 }
