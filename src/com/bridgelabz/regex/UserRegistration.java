2c5d5ba046fe419a07d4f129b0a85028912f30b9
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2023-02-06 23:31:53 +0530

    Added: To Validation of First Name

--- src/com/bridgelabz/regex/UserRegistration.java
+++ src/com/bridgelabz/regex/UserRegistration.java
@@ -1,8 +1,21 @@
 package com.bridgelabz.regex;
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
+
+	public boolean userName(String regex, String input) {
+		return Pattern.compile(regex).matcher(input).matches();
+	}
 
 	public static void main(String[] args) {
 		System.out.println("welcome to User Registration program");
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter First Name: ");
+		String firstName = "[A-Z]{1}[a-z]{2,}";
+		String input = scanner.next(); // Nadeem = ture, nadeem = false
+		UserRegistration userRegistration = new UserRegistration();
+		System.out.println(userRegistration.userName(firstName, input));
 	}
 }
